c176c24772d305c69d387085e8740a376164742d
SanguineHaze <Sanguinehaze@gmail.com>
2016-08-24 14:03:35 -0600

    0.1
    Base starting

---
+++ src/race.java
@@ -0,0 +1,33 @@
+
+public class race {
+	race(){
+			
+	String[] raceArray = new String[10];
+	
+	raceArray[0] = "Dragonborn";
+	raceArray[1] = "Dwarf";
+	raceArray[2] = "Elf";
+	raceArray[3] = "Genasi";
+	raceArray[4] = "Gnome";
+	raceArray[5] = "Goliath";
+	raceArray[6] = "Halfling";
+	raceArray[7] = "Half-Elf";
+	raceArray[8] = "Half-Orc";
+	raceArray[9] = "Human";
+	raceArray[10] = "Tiefling";
+	}
+	
+	void subRace(){
+		String[] subRaceArray = new String[22];
+		
+		//Dwarves
+		subRaceArray[0] = "Hill Dwarf"; 
+		subRaceArray[1] = "Mountain Dwarf"; 
+		subRaceArray[2] = "Duergar"; 
+		//Elves
+		subRaceArray[3]	= "Drow"; 
+		subRaceArray[4] = "Eladrin";
+		subRaceArray[5] = "High Elf";
+		subRaceArray[6] = "Wood Elf";
+	}
+}
