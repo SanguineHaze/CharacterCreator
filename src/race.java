77ebcc2e61e56aad46f6e12bba2460e76d5e7229
SanguineHaze <Sanguinehaze@gmail.com>
2016-09-08 12:40:06 -0600

    0.2.1
    Cleaned up old comments

--- src/race.java
+++ src/race.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.util.Vector;
 
 
 public class race {
+	Vector<String> raceList = new Vector<String>(10);
+	
 	race() throws IOException {
 		String targetFile = "C:/users/dylanc/Desktop/test.txt";
 		
@@ -21,51 +24,8 @@ public class race {
 			
 			//Let's make our own error
 			//System.out.println("ERROR: Dude, where's my file?! Better check that spelling!");
-		}
-	}
-	
-	
-	
-	//Keeping this to "show our work", lol.
-	/*race(){
-			
-	String[] raceArray = new String[10];
-	
-	raceArray[0] = "Dragonborn";
-	raceArray[1] = "Dwarf";
-	raceArray[2] = "Elf";
-	raceArray[3] = "Genasi";
-	raceArray[4] = "Gnome";
-	raceArray[5] = "Goliath";
-	raceArray[6] = "Halfling";
-	raceArray[7] = "Half-Elf";
-	raceArray[8] = "Half-Orc";
-	raceArray[9] = "Human";
-	raceArray[10] = "Tiefling";
-	}*/
-	
-	/*public enum Race {
-		Dragonborn, Dwarf, Elf, Genasi, Gnome, Goliath, Halfling, Half-Elf, Half-Orc, Human, Tiefling;
-	}*/
-	
-	//It doesn't like the dashes in Half-Elf. Talking with Blair, he suggested that perhaps the best method would indeed be to load from file. Therefore, I'm going to do so.
-	
-	
-	/*void subRace(){
-		String[] subRaceArray = new String[22];
+		} //End catch
 		
-		//Dwarves
-		subRaceArray[0] = "Hill Dwarf";
-		subRaceArray[1] = "Mountain Dwarf"; 
-		subRaceArray[2] = "Duergar"; 
-		//Elves
-		subRaceArray[3]	= "Drow"; 
-		subRaceArray[4] = "Eladrin";
-		subRaceArray[5] = "High Elf";
-		subRaceArray[6] = "Wood Elf";
-	}*/
+	}//End race()
 	
-	//Lunar suggests looking into Enums - as there may be a better way to do this.
-	
-	//I should also look into how to load from a file. http://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java
-}
+}//End class
