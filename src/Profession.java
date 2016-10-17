44826fc8860fb601d868cdba06907fccb594c00f
SanguineHaze <Sanguinehaze@gmail.com>
2016-10-17 16:28:59 -0600

    0.5.5.1 - Professions.txt updated
    More professions. A few more notes as well.

--- src/Profession.java
+++ src/Profession.java
@@ -27,6 +27,7 @@ public class Profession {
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
 		//DEBUG TOOL - Show the list
 		//System.out.println(professionList);
 		
@@ -35,6 +36,8 @@ public class Profession {
 	
 	//generateProfession()
 		//This *should* encompass classes. A vast list ensures we don't have a proliferation of classed NPCs (as hero types are rare)
+		//Upon creating a basic list of some classes & jobs, I'm going to need to handle sub-classes and other features it looks like.
+			//alternatively, I could declare them in the file itself. "Paladin of Selune" etc.
 	
 	//generateAlignment()
 	
