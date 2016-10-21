270641270804b7f054707401264157cbcaca97bf
SanguineHaze <Sanguinehaze@gmail.com>
2016-10-21 16:53:00 -0600

    0.6.3 - Prelim work for user-defined selections

--- src/CharacterCreator.java
+++ src/CharacterCreator.java
@@ -1,19 +1,21 @@
+import java.util.Scanner;
 
 public class CharacterCreator {
     public static void main(String[] args) {
-	    //Note to self: Consider adding each of these results to an arrayList, which can then just print everything at once. (17,Oct,2016)
+	    //Note to self: Consider adding each of these results to an arrayList, which can then just print everything at once. (17,Oct,2016)	
+    	
 		for(int i = 0; i < 30; i++){	
 		//RACE SECTION    	
 	        Race thisRace = null; 
 			thisRace = new Race(); 
-			String myRace = thisRace.getRace();
+				//thisRace.chosenRace = ""; //Define a desired race to output
+			String myRace = thisRace.chosenRace;
 			//Check if the race is being assigned
 			System.out.println("Race: " + myRace); 
 			
 		//SUBRACE SECTION			
 	    	SubRace thisSubRace = null;
 			thisSubRace = new SubRace();
-			//use thisSubRace's getChosenRace method to look at thisRace.getRace method. Thus passing in whatever variable has been assigned it (as chosenRace)
 			String mySubRace = thisSubRace.getChosenRace(myRace);
 			if(mySubRace != ""){
 				System.out.println("Subrace: " + mySubRace);
@@ -22,10 +24,12 @@ public class CharacterCreator {
 		//NAME (& SEX & AGE) SECTION			
 			Name thisName = null;
 			thisName = new Name();
+				//thisName.sex = ""; //Define a desired sex to output
 			String mySex = thisName.sex;
 			System.out.println("Sex: " + mySex);
 			String myName = thisName.fullName;
 			System.out.println("Name: " + myName);
+				//thisName.chosenAge = ""; //Define a desired age to output
 			String myAge = thisName.chosenAge;
 			System.out.println("Age: " + myAge);
 			
@@ -35,8 +39,8 @@ public class CharacterCreator {
 			String myProfession = thisProfession.chosenProfession;
 			System.out.println("Profession: " + myProfession);
 			
-		//Linesplit for multiple result tidiness
+		//Line-split for multiple result tidiness
 			System.out.println("");
-		}//end for loop
+		}//end FOR loop
     }// End main()    
 }
