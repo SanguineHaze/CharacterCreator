4643ab636d011db0747b153b4afeff2049e74367
SanguineHaze <Sanguinehaze@gmail.com>
2016-10-26 16:56:47 -0600

    0.7.2.2 - Minor improvements to Additional Features

--- src/AdditionalFeatures.java
+++ src/AdditionalFeatures.java
@@ -1,17 +1,51 @@
+import java.util.ArrayList;
 
 public class AdditionalFeatures {
 	
 	String motivation;
 	String personality;
 	String nickname; //might remove nickname, but for now, it's in.
+	ArrayList<String> motivationList = new ArrayList<String>();
+	ArrayList<String> personalityList = new ArrayList<String>();
 	
 	public AdditionalFeatures(String chosenAge, String chosenSex, String chosenProfession){
 		motivation = "";
 		personality = "";
 		nickname = "";
+		loadMotivationList();
+		loadPersonalityList();
 	}
 	
-	//Will need to read a file for motivations, personalities, and nicknames.
+	private void loadMotivationList(){
+		String motivationListTargetFile = "src/motivations.txt";
+		
+		try {
+			ReadFromFile file = new ReadFromFile(motivationListTargetFile);
+			motivationList = file.OpenFile();
+			
+			//DEBUG TOOL: Check to see that the list is being created
+		    /*
+		    System.out.println("Motivation List:");
+		    for(String out: motivationList){
+		        System.out.println(out);
+		    }
+		    */
+			
+		} catch(Exception e) {
+			 System.out.println(e.getMessage());
+		}//end Try/Catch
+	}//end loadMotivationList()
+	
+	private void loadPersonalityList() {
+		String personalityListTargetFile = "src/personalities.txt";
+		
+		try {
+			ReadFromFile file = new ReadFromFile(personalityListTargetFile);
+			personalityList = file.OpenFile();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}//end Try/Catch
+	}//end loadPersonalityList()
 	
 	//Will need to construct each result
 	
