fcb68d5d16102d758fccd9b483d23dcda80b7bb5
Blair Stewart <blair.stewart@gmail.com>
2017-09-01 14:04:14 -0600

    * Pulled CharacterResult Generation out of MainFrame * Cleaned up Warnings

--- src/sanguinehaze/charactercreator/adapter/views/factories/MainFrameFactory.java
+++ src/sanguinehaze/charactercreator/adapter/views/factories/MainFrameFactory.java
@@ -2,14 +2,28 @@ package sanguinehaze.charactercreator.adapter.views.factories;
 
 import sanguinehaze.charactercreator.GenerateSourceData;
 import sanguinehaze.charactercreator.adapter.views.MainFrame;
+import sanguinehaze.charactercreator.adapter.views.mappers.CharacterResultViewModelMapper;
+import sanguinehaze.charactercreator.adapter.views.viewmodels.CharacterResultViewModel;
+import sanguinehaze.charactercreator.application.CharacterResultProvider;
+import sanguinehaze.charactercreator.domain.CharacterCreatorRandom;
 import sanguinehaze.charactercreator.domain.NameBuilder;
 import sanguinehaze.charactercreator.adapter.persistence.SourcePersistence;
 
 public class MainFrameFactory {
 
+    @SuppressWarnings("UnusedReturnValue")
     public static MainFrame create(SourcePersistence sourcePersistance) {
+        CharacterCreatorRandom rand = new CharacterCreatorRandom();
         GenerateSourceData sourceData = new GenerateSourceData(sourcePersistance);
         NameBuilder nameBuilder = new NameBuilder(sourceData);
-        return new MainFrame(sourceData, nameBuilder);
+        CharacterResultViewModelMapper mapper = new CharacterResultViewModelMapper();
+        CharacterResultProvider provider = new CharacterResultProvider(rand);
+
+        return new MainFrame(
+                sourceData,
+                nameBuilder,
+                mapper,
+                provider,
+                rand);
     }
 }
